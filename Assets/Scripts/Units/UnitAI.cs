42ab412a1deb6a4adc512bb20ad13742522749a8
wiyadathi <wiyada.t@bu.ac.th>
2024-03-27 21:36:54 +0700

    FortTower Ghost Building

--- Assets/Scripts/Units/UnitAI.cs
+++ Assets/Scripts/Units/UnitAI.cs
@@ -96,7 +96,59 @@ public class UnitAI : MonoBehaviour
                     unit.ToAttackUnit(enemy);
                 }
             }
+            else
+            {
+                Building potentialEnemyBuilding = CheckForNearbyEnemyBuildings();
+
+                if (potentialEnemyBuilding != null)
+                {
+                    unit.ToAttackBuilding(potentialEnemyBuilding);
+                }
+            }
         }
     }
 
+    // checks for nearby enemy buildings with a sphere cast
+    protected Building CheckForNearbyEnemyBuildings()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, unit.DefendRange, Vector3.up, buildingLayerMask);
+
+        GameObject closest = null;
+        float closestDist = 0.0f;
+
+        for (int x = 0; x < hits.Length; x++)
+        {
+            //Debug.Log("Test - " + hits[x].collider.gameObject.ToString());
+            Building target = hits[x].collider.GetComponent<Building>();
+
+            // skip if this is not a building or destroyed
+            if ((target == null) || (target.CurHP <= 0))
+                continue;
+
+            // skip if it is a natural/neutral Building
+            if (target.Faction == null)
+                continue;
+
+            // is this my building?
+            else if (unit.Faction.IsMyBuilding(target))
+                continue;
+
+            // if it is not the closest enemy building or the distance is less than the closest distance it currently has
+            else if (!closest || (Vector3.Distance(transform.position, hits[x].transform.position) < closestDist))
+            {
+                closest = hits[x].collider.gameObject;
+                closestDist = Vector3.Distance(transform.position, hits[x].transform.position);
+            }
+        }
+
+        if (closest != null)
+        {
+            //Debug.Log(closest.gameObject.ToString() + ", " + closestDist.ToString());
+            return closest.GetComponent<Building>();
+        }
+        else
+            return null;
+    }
+
+    
 }
